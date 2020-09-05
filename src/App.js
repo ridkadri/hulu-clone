b442cfce658bdb3bc4c663f9ba7a70dd54facf3f
Ridwan <kadriridwan0@gmail.com>
2020-09-05 13:06:03 +0000

    added navigation UI elements and took out visible scrollbar off

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header/Header';
+import Nav from './components/Navigation/Nav';
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Nav/>
       {/*Header*/}
       {/*Nav*/}
       {/*Result*/}
