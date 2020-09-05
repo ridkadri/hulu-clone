9211ab4cafdef74aa05e10f8670b7729625509c0
Ridwan <kadriridwan0@gmail.com>
2020-09-05 12:39:19 +0000

    built header component with Material UI icons and css

---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Header.css';
+
+//icons
+import HomeIcon from '@material-ui/icons/Home';
+import FlashIcon from '@material-ui/icons/FlashOn';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header_icons'>
+                <div className='header_icon header_icon--active'>
+                <HomeIcon/>
+                <p>Home</p>
+                </div>
+
+                <div className='header_icon'>
+                <FlashIcon/>
+                <p>Trending</p>
+                </div>
+
+                <div className='header_icon'>
+                <LiveTvIcon/>
+                <p>Verified</p>
+                </div>
+
+                <div className='header_icon'>
+                <VideoLibraryIcon/>
+                <p>Collections</p>
+                </div>
+
+                <div className='header_icon'>
+                <SearchIcon/>
+                <p>Search</p>
+                </div>
+
+                <div className='header_icon'>
+                <PersonOutlineIcon/>
+                <p>Account</p>
+                </div>
+            </div>
+            <img src='https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png' alt=''/>
+        </div>
+    )
+}
+
+export default Header;
