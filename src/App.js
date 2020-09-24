f8133317c80a74af8627f6c3476369fcffc8804e
Ridwan <kadriridwan0@gmail.com>
2020-09-24 16:48:07 +0000

    added movie database api to render movies, added navigation links to show exact genres, added flip-move for animatedcard flips

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 import Header from './components/Header/Header';
 import Nav from './components/Navigation/Nav';
+import Result from './components/Results/Results';
+
+import requests from './requests';
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
   return (
     <div className="app">
       <Header/>
-      <Nav/>
-      {/*Header*/}
-      {/*Nav*/}
-      {/*Result*/}
+
+      <Nav setSelectedOption={setSelectedOption}/>
+
+      <Result selectedOption={selectedOption}/>
     </div>
   );
 }
